1bd79cd54f519c6cba5aea6e393648dd4a20a8c7
DhruveshShyara <dhruveshshyara33@gmail.com>
2025-02-25 03:42:21 +0530

    update users_design_data

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,16 @@ app.use(cors({
     credentials: true
 }));
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://shyara-gold.netlify.app");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+
+
 
 const PORT = process.env.PORT || 4001;
 
